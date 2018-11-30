6ae3d26dcd838ef139072261c32f777bf9103508
junhan-kim <kjune1236@gmail.com>
2018-11-30 13:33:25 +0900

    Fix text encoding error

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,29 @@
+// https://github.com/CedricGuillemet/Imogen
+//
+// The MIT License(MIT)
+// 
+// Copyright(c) 2018 Cedric Guillemet
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+
 #include "imgui.h"
 #include "imgui_internal.h"
 #include "imgui_impl_sdl.h"
@@ -11,6 +37,9 @@
 #include "NodesDelegate.h"
 #include "Evaluation.h"
 #include "Imogen.h"
+#include "TaskScheduler.h"
+#include "stb_image.h"
+#include "stb_image_write.h"
 
 // ------------------------------------- 머리 주석 ---------------------------------
 
@@ -22,7 +51,8 @@
 // 이력 사항 : main.cpp의 줄단위 주석 및 머리주석 추가 (김준한, 2018-10-31) 
 
 
-
+TileNodeEditGraphDelegate *TileNodeEditGraphDelegate::mInstance = NULL;
+unsigned int gCPUCount = 1;
 int Log(const char *szFormat, ...) // 가변인자를 받아 로그를 기록하는 함수
 {
 	va_list ptr_arg;
