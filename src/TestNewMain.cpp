86617efb631b4b5d8d7af3090ff3164c8c0f0fa3
Hyeon Woo Jeong <qlgks1@naver.com>
2018-12-03 18:55:55 +0900

    New version에서 따온 비교전용 (전체주석) main code

---
+++ src/TestNewMain.cpp
@@ -0,0 +1,278 @@
+﻿// https://github.com/CedricGuillemet/Imogen
+//
+// The MIT License(MIT)
+// 
+
+/*
+- new version of imogen, mian
+- hw forked
+- python을 도입하면서 근본적인 class가 좀 변동이 있음
+- 반영을 하긴 좀 힘들듯,, 하지만 new로 새롭게 바뀐 부분을 살펴보면 좋을 듯!
+
+- 전체 주석 처리 해놓을 껏
+
+- 특히 빠진 헤더와 이름 변경된 경로 파일들이 존재함, 원래 원본 저장소 참조 필수
+*/
+
+
+//#include "imgui.h"
+//#include "imgui_internal.h"
+//#include "imgui_impl_sdl.h"
+//#include "imgui_impl_opengl3.h"
+//#include <SDL.h>
+//#include <GL/gl3w.h>
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <io.h> 
+//#include <fcntl.h> 
+//#include "Nodes.h"
+//#include "NodesDelegate.h"
+//#include "Evaluation.h"
+//#include "Imogen.h"
+//#include "TaskScheduler.h"
+//#include "stb_image.h"
+//#include "stb_image_write.h"
+//#include "ffmpegCodec.h"
+//#include "Evaluators.h"
+//#include "cmft/clcontext.h"
+//#include "cmft/clcontext_internal.h"
+//
+//unsigned int gCPUCount = 1;
+//cmft::ClContext* clContext = NULL;
+//
+//void APIENTRY openglCallbackFunction(GLenum /*source*/,
+//	GLenum type,
+//	GLuint id,
+//	GLenum severity,
+//	GLsizei /*length*/,
+//	const GLchar* message,
+//	const void* /*userParam*/)
+//{
+//	const char *typeStr = "";
+//	const char *severityStr = "";
+//
+//	switch (type)
+//	{
+//	case GL_DEBUG_TYPE_ERROR:
+//		typeStr = "ERROR";
+//		break;
+//	case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
+//		typeStr = "DEPRECATED_BEHAVIOR";
+//		break;
+//	case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
+//		typeStr = "UNDEFINED_BEHAVIOR";
+//		break;
+//	case GL_DEBUG_TYPE_PORTABILITY:
+//		typeStr = "PORTABILITY";
+//		break;
+//	case GL_DEBUG_TYPE_PERFORMANCE:
+//		typeStr = "PERFORMANCE";
+//		break;
+//	case GL_DEBUG_TYPE_OTHER:
+//		typeStr = "OTHER";
+//		// skip
+//		return;
+//		break;
+//	}
+//
+//	switch (severity)
+//	{
+//	case GL_DEBUG_SEVERITY_LOW:
+//		severityStr = "LOW";
+//		return;
+//		break;
+//	case GL_DEBUG_SEVERITY_MEDIUM:
+//		severityStr = "MEDIUM";
+//		break;
+//	case GL_DEBUG_SEVERITY_HIGH:
+//		severityStr = "HIGH";
+//		break;
+//	}
+//	Log("GL Debug (%s - %s) %s \n", typeStr, severityStr, message);
+//}
+//
+//Evaluation gEvaluation;
+//Library library;
+//Imogen imogen;
+//enki::TaskScheduler g_TS;
+//
+//int main(int, char**)
+//{
+//	g_TS.Initialize();
+//	pybind11::scoped_interpreter guard{}; // start the interpreter and keep it alive
+//	LoadMetaNodes();
+//	FFMPEGCodec::RegisterAll();
+//	FFMPEGCodec::Log = Log;
+//
+//	stbi_set_flip_vertically_on_load(1);
+//	stbi_flip_vertically_on_write(1);
+//
+//	// Setup SDL
+//	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0)
+//	{
+//		printf("Error: %s\n", SDL_GetError());
+//		return -1;
+//	}
+//
+//	// Decide GL+GLSL versions
+//#if __APPLE__
+//	// GL 3.2 Core + GLSL 150
+//	const char* glsl_version = "#version 150";
+//	SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG); // Always required on Mac
+//	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+//	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+//	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+//#else
+//	// GL 3.0 + GLSL 130
+//	const char* glsl_version = "#version 130";
+//	SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_DEBUG_FLAG);
+//	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+//	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+//	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+//#endif
+//
+//	// Create window with graphics context
+//	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+//	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+//	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+//	SDL_DisplayMode current;
+//	SDL_GetCurrentDisplayMode(0, &current);
+//	SDL_Window* window = SDL_CreateWindow("Imogen 0.6.0", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_MAXIMIZED);
+//	SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+//	SDL_GL_SetSwapInterval(1); // Enable vsync
+//
+//	// Initialize OpenGL loader
+//#if defined(IMGUI_IMPL_OPENGL_LOADER_GL3W)
+//	bool err = gl3wInit() != 0;
+//#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLEW)
+//	bool err = glewInit() != GLEW_OK;
+//#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD)
+//	bool err = gladLoadGL() == 0;
+//#endif
+//	if (err)
+//	{
+//		fprintf(stderr, "Failed to initialize OpenGL loader!\n");
+//		return 1;
+//	}
+//
+//	// open cl context
+//	int32_t clLoaded = 0;
+//	clLoaded = cmft::clLoad();
+//	if (clLoaded)
+//	{
+//		uint32_t clVendor = (uint32_t)CMFT_CL_VENDOR_ANY_GPU;
+//		uint32_t deviceType = CMFT_CL_DEVICE_TYPE_GPU;
+//		uint32_t deviceIndex = 0;
+//		clContext = cmft::clInit(clVendor, deviceType, deviceIndex);
+//	}
+//	if (clLoaded && clContext)
+//	{
+//		Log("OpenCL context created with %s / %s\n", clContext->m_deviceVendor, clContext->m_deviceName);
+//	}
+//	else
+//	{
+//		Log("OpenCL context not created.\n");
+//	}
+//
+//	// Setup Dear ImGui binding
+//	IMGUI_CHECKVERSION();
+//	ImGui::CreateContext();
+//	ImGuiIO& io = ImGui::GetIO(); (void)io;
+//	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
+//
+//	ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
+//	ImGui_ImplOpenGL3_Init(glsl_version);
+//
+//	// opengl debug
+//	glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+//	glDebugMessageCallback((GLDEBUGPROCARB)openglCallbackFunction, NULL);
+//	GLuint unusedIds = 0;
+//	glDebugMessageControl(GL_DONT_CARE,
+//		GL_DONT_CARE,
+//		GL_DONT_CARE,
+//		0,
+//		&unusedIds,
+//		true);
+//
+//	gFSQuad.Init();
+//
+//	// Setup style
+//	ImGui::StyleColorsDark();
+//
+//	static const char* libraryFilename = "library.dat";
+//
+//	LoadLib(&library, libraryFilename);
+//
+//	imogen.Init();
+//
+//	gEvaluation.Init();
+//	gEvaluators.SetEvaluators(imogen.mEvaluatorFiles);
+//
+//	gCPUCount = SDL_GetCPUCount();
+//
+//	// Main loop
+//	bool done = false;
+//	while (!done)
+//	{
+//		SDL_Event event;
+//		while (SDL_PollEvent(&event))
+//		{
+//			ImGui_ImplSDL2_ProcessEvent(&event);
+//			if (event.type == SDL_QUIT)
+//				done = true;
+//			if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE && event.window.windowID == SDL_GetWindowID(window))
+//				done = true;
+//		}
+//		// undo/redo
+//		if (io.KeyCtrl && ImGui::IsKeyPressedMap(ImGuiKey_Z))
+//			gUndoRedoHandler.Undo();
+//		if ((io.KeyCtrl && io.KeyShift && ImGui::IsKeyPressedMap(ImGuiKey_Z)) ||
+//			(io.KeyCtrl && ImGui::IsKeyPressedMap(ImGuiKey_Y)))
+//			gUndoRedoHandler.Redo();
+//
+//		// Start the Dear ImGui frame
+//		ImGui_ImplOpenGL3_NewFrame();
+//		ImGui_ImplSDL2_NewFrame(window);
+//		ImGui::NewFrame();
+//		InitCallbackRects();
+//
+//		gCurrentContext->RunDirty();
+//		imogen.Show(library, gNodeDelegate, gEvaluation);
+//
+//		// render everything
+//		glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
+//		glClearColor(0., 0., 0., 0.);
+//		glClear(GL_COLOR_BUFFER_BIT);
+//
+//		ImGui::Render();
+//		SDL_GL_MakeCurrent(window, gl_context);
+//		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+//		g_TS.RunPinnedTasks();
+//		SDL_GL_SwapWindow(window);
+//	}
+//
+//	clDestroy(clContext);
+//	// Unload opencl lib.
+//	if (clLoaded)
+//	{
+//		cmft::clUnload();
+//	}
+//
+//	imogen.ValidateCurrentMaterial(library, gNodeDelegate);
+//	SaveLib(&library, libraryFilename);
+//	gEvaluation.Finish();
+//
+//	// Cleanup
+//	ImGui_ImplOpenGL3_Shutdown();
+//	ImGui_ImplSDL2_Shutdown();
+//	ImGui::DestroyContext();
+//
+//	imogen.Finish(); // keep dock being saved
+//
+//	SDL_GL_DeleteContext(gl_context);
+//	SDL_DestroyWindow(window);
+//	SDL_Quit();
+//
+//	g_TS.WaitforAllAndShutdown();
+//	return 0;
+//}
